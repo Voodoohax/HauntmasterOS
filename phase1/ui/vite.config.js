970dd02368cf55d750bc37d5d2c11de7521d1b57
Voodoohax <33105130+Voodoohax@users.noreply.github.com>
2025-11-13 12:53:23 -0600

    Update vite.config.js

--- phase1/ui/vite.config.js
+++ phase1/ui/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',  // ← API runs here
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
