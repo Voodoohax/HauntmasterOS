3881bf2afb5e335b7c276a13577b8280b785675d
Voodoohax <33105130+Voodoohax@users.noreply.github.com>
2025-11-13 19:02:22 -0600

    Create vite.config.js

---
+++ phase1/composer/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/media': 'http://localhost:3000',
+      '/thumbs': 'http://localhost:3000',
+    },
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
